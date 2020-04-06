0f44fdb112c618ab88ff75223e657cb31b77bca4
Xandru Mifsud <xandru.mifsud@gmail.com>
2020-04-06 18:45:57 +0200

    Expanding on HelloWorld example class and testers, to test Jenkins setup.

--- src/main/java/com/xd/cps2002/HelloWorld.java
+++ src/main/java/com/xd/cps2002/HelloWorld.java
@@ -1,5 +1,23 @@
 package com.xd.cps2002;
 
 public class HelloWorld{
-    String hw = "Hello World!\n";
+    private static String hw = "Hello World!\n";
+
+    // simply returns the greeting "Hello World!\n" exactly once
+    public static String greeting(){
+        return hw;
+    }
+
+    // returns the greeting "Hello World!\n" n times for n >= 0, else throws an exception
+    public static String multi_greeting(int n) throws Exception{
+        if(n < 0){
+            throw new Exception("Invalid Input: Non-negative input only accepted.");
+        }
+        else if(n==0){
+            return "";
+        }
+        else{
+            return new String(new char[5]).replace("\0", hw);
+        }
+    }
 }
--- src/test/java/com/xd/cps2002/HelloWorld_Test.java
+++ src/test/java/com/xd/cps2002/HelloWorld_Test.java
@@ -12,16 +12,20 @@ public class HelloWorld_Test{
         assertEquals(hw, HelloWorld.greeting());
     }
 
-    /* simple test to verify that the multi_greeting function returns n Hello World!\n statement, and handles
-     * erroneous input correctly.
-     */
+    // simple test to verify that the multi_greeting function returns n Hello World!\n statement
     @Test
-    public void multi_greetingTest(){
+    public void multi_greetingTest() throws Exception{
         // testing the case when 0 is specified for n
         assertEquals("", HelloWorld.multi_greeting(0));
 
         // testing the case when a valid integer is provided
         String repeated = new String(new char[5]).replace("\0", hw);
         assertEquals(repeated, HelloWorld.multi_greeting(5));
+    }
+
+    // simple test to verify that multi_greeting throws an exception when an invalid input is supplied
+    @Test(expected = Exception.class)
+    public void multi_greetingThrowsETest() throws Exception {
+        String s = HelloWorld.multi_greeting(-1);
     }
 }
