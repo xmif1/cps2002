66f3105641973163815503e485568b795446b30d
Xandru Mifsud <xandru.mifsud@gmail.com>
2020-04-20 16:59:50 +0200

    Added new unit tests, eliminated specification of map type [beyond current scope]

--- src/test/java/com/xd/cps2002/MainGameTest.java
+++ src/test/java/com/xd/cps2002/MainGameTest.java
@@ -3,6 +3,7 @@ package com.xd.cps2002;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import sun.rmi.rmic.Main;
 
 import static org.junit.Assert.*;
 
@@ -67,7 +68,7 @@ public class MainGameTest{
      * @throws InvalidNumberOfPlayersException is thrown if the number of players is < 2 or > 8 (not expected).
      */
     @Test(expected = SetupOperationPrecedenceException.class)
-    public void mapInitialized_beforePlayersTest() throws SetupOperationPrecedenceException,
+    public void mapInitializedBeforePlayers_setupPlayersTest() throws SetupOperationPrecedenceException,
             InvalidNumberOfPlayersException{
 
         MainGame.map = new BasicMap(tiles);
@@ -78,42 +79,36 @@ public class MainGameTest{
      * Tests that a SetupOperationPrecedenceException is thrown when no players have been initialized before Map init.
      * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (expected).
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (not expected).
-     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = SetupOperationPrecedenceException.class)
-    public void noPlayersInitialized_setupMapTest() throws SetupOperationPrecedenceException, InvalidMapSizeException,
-            IllegalArgumentException{
+    public void noPlayersInitialized_setupMapTest() throws SetupOperationPrecedenceException, InvalidMapSizeException{
 
         MainGame.players = null;
-        mainGame.setupMap("basic",8);
+        mainGame.setupMap(8);
     }
 
     /**
      * Tests that an InvalidMapSizeException is thrown when the map_size is less than 5.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
      * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
-     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void belowMin_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
-            IllegalArgumentException{
+    public void belowMin_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException{
 
         int map_size = 4;
-        mainGame.setupMap("basic", map_size);
+        mainGame.setupMap(map_size);
     }
 
     /**
      * Tests that an InvalidMapSizeException is thrown when the map_size is greater than 50.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
      * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
-     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void aboveMax_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
-            IllegalArgumentException{
+    public void aboveMax_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException{
 
         int map_size = 51;
-        mainGame.setupMap("basic", map_size);
+        mainGame.setupMap(map_size);
     }
 
     /**
@@ -121,16 +116,49 @@ public class MainGameTest{
      * initialized players.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
      * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
-     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void smallMap_Min5Players_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
-            IllegalArgumentException{
+    public void smallMap_Min5Players_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException{
 
         MainGame.players = new Player[5];
 
         int map_size = 5;
-        mainGame.setupMap("basic", map_size);
+        mainGame.setupMap(map_size);
+    }
+
+    /**
+     * Tests that a SetupOperationPrecedenceException is thrown when no Players have been initialized.
+     * @throws SetupOperationPrecedenceException is thrown if no Players or Map has been initialized (expected).
+     */
+    @Test(expected = SetupOperationPrecedenceException.class)
+    public void noPlayersInitialized_setPlayerPositionsTest() throws SetupOperationPrecedenceException{
+        MainGame.map = new BasicMap(tiles);
+
+        MainGame.players = null;
+        mainGame.setPlayerPositions();
+    }
+
+    /**
+     * Tests that a SetupOperationPrecedenceException is thrown when no Map instance has been initialized.
+     * @throws SetupOperationPrecedenceException is thrown if no Players or Map has been initialized (expected).
+     */
+    @Test(expected = SetupOperationPrecedenceException.class)
+    public void noMapInitialized_setPlayerPositionsTest() throws SetupOperationPrecedenceException{
+        MainGame.players = new Player[5];
+
+        MainGame.map = null;
+        mainGame.setPlayerPositions();
+    }
+
+    /**
+     * Asserts that the MainGame.players and MainGame.map are set to null.
+     */
+    @Test
+    public void mainGame_resetTest(){
+        mainGame.reset();
+
+        assertNull(MainGame.players);
+        assertNull(MainGame.map);
     }
 
     @After
