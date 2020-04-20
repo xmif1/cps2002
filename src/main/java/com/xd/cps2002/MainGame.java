c2e100090abb50d4ded97df1c07395a1b2608846
Xandru Mifsud <xandru.mifsud@gmail.com>
2020-04-20 16:30:20 +0200

    Improving MainGame interfacing to prevent incorrect order of operations during setup.

--- src/main/java/com/xd/cps2002/MainGame.java
+++ src/main/java/com/xd/cps2002/MainGame.java
@@ -2,7 +2,10 @@ package com.xd.cps2002;
 
 /**
  * The MainGame class is responsible for coordinating the main logic of the game, and the primary interface for user
- * input and provision of output to the user.
+ * input and provision of output to the user. It implements a Singleton design pattern, and is designed to be the
+ * primary interface to the game API, providing descriptive and detailed errors. Moreover, it handles the majority of
+ * setup and is designed to prevent the execution of setup steps in an invalid procedural order, if done so through this
+ * class. Otherwise access to the sub-systems is left unhindered by this class.
  *
  * @author Xandru Mifsud
  */
@@ -38,13 +41,16 @@ public class MainGame{
     /**
      * Initializes n_players Player instances, if n_players is not less than 2 or greater then 8.
      * @param n_players is the number of players to be initialized.
-     * @return boolean true if MainGame.players has been set.
+     * @throws SetupOperationPrecedenceException is thrown if a Map instances has already been created.
      * @throws InvalidNumberOfPlayersException is thrown if the number of players is < 2 or > 8.
      */
-    public boolean setupPlayers(int n_players) throws InvalidNumberOfPlayersException{
-        if(2 <= n_players && n_players <= 8){ // validation check
+    public void setupPlayers(int n_players) throws SetupOperationPrecedenceException, InvalidNumberOfPlayersException{
+        if(map != null){
+            throw new SetupOperationPrecedenceException("Invalid attempt to setup Player instances after a Map instance" +
+                    " has already been initialized.");
+        }
+        else if(2 <= n_players && n_players <= 8){ // validation check
             players = new Player[n_players]; // initialize if within range
-            return true;
         }
         else{
             throw new InvalidNumberOfPlayersException(n_players); // else throw exception
@@ -52,18 +58,21 @@ public class MainGame{
     }
 
     /**
-     * Initializes a Map of size map_size, provided that:
+     * Initializes a Map of type map_type and size map_size, provided that:
      * i.   Any players in the game have been initialized (i.e. MainGame.players is not null)
      * ii.  The map_size is not less than 5 and not greater than 50.
      * iii. If the number of players is at least 5, than the minimum map_size is at least 8.
+     * iv.  The map_type corresponds to a defined concrete map implementation, as established by the MapCreator.
      * @param map_size is the size of the map.
-     * @return boolean true if MainGame.map has been set.
-     * @throws InvalidNumberOfPlayersException is thrown if MainGame.players is null. [Criteria i. above]
+     * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null. [Criteria i. above]
      * @throws InvalidMapSizeException is thrown if the map_size is invalid. [Criteria ii. & iii. above]
+     * @throws IllegalArgumentException is thrown whenever the map type specified is invalid. [Criteria iv. above]
      */
-    public boolean setupMap(int map_size) throws InvalidNumberOfPlayersException, InvalidMapSizeException{
+    public void setupMap(String map_type, int map_size) throws SetupOperationPrecedenceException, InvalidMapSizeException,
+            IllegalArgumentException{
+
         if(players == null){ // if players have not been initialized
-            throw new InvalidNumberOfPlayersException(0);
+            throw new SetupOperationPrecedenceException("Invalid attempt to setup Map instance before Player instances.");
         }
         else if(51 <= map_size || map_size <= 4){ // else if map_size is outside the global minimum and maximum size
             throw new InvalidMapSizeException(map_size);
@@ -72,21 +81,23 @@ public class MainGame{
             throw new InvalidMapSizeException(map_size, "For 5 to 8 players, the minimum map size is 8x8.");
         }
         else{ // else initialize map
-            // TO-DO
-            return true;
+            do{
+                map = MapCreator.createMap(map_type, map_size);
+            } while(!map.isPlayable()); // attempt map creation until generated map is playable
         }
+    }
+}
+
+class SetupOperationPrecedenceException extends Exception{
+    public SetupOperationPrecedenceException(String s){
+        System.err.println("Setup Operations Executed In Wrong Sequence: " + s);
     }
 }
 
 class InvalidNumberOfPlayersException extends Exception{
     public InvalidNumberOfPlayersException(int n_players){
-        if(n_players == 0){
-            System.err.println("No players have been initialised.");
-        }
-        else{
-            System.err.println("Invalid input provided: Cannot have " + n_players + " players." +
-                    " The minimum number of players is 2 while the maximum is 8.");
-        }
+        System.err.println("Invalid input provided: Cannot have " + n_players + " players." +
+                " The minimum number of players is 2 while the maximum is 8.");
     }
 }
 
--- src/test/java/com/xd/cps2002/MainGameTest.java
+++ src/test/java/com/xd/cps2002/MainGameTest.java
@@ -13,6 +13,13 @@ import static org.junit.Assert.*;
 public class MainGameTest{
     private final MainGame mainGame = MainGame.getMainGame();
 
+    // define the Map by means of a 2D TileType array
+    private final TileType[][] tiles = {{TileType.Grass, TileType.Grass, TileType.Grass, TileType.Grass, TileType.Grass},
+                                        {TileType.Grass, TileType.Grass, TileType.Water, TileType.Water, TileType.Water},
+                                        {TileType.Grass, TileType.Grass, TileType.Treasure, TileType.Water, TileType.Water},
+                                        {TileType.Grass, TileType.Grass, TileType.Grass, TileType.Water, TileType.Water},
+                                        {TileType.Grass, TileType.Grass, TileType.Grass, TileType.Grass, TileType.Water}};
+
     @Before
     public void setupMainGameTest(){
         MainGame.players = new Player[2]; // initialize two players
@@ -31,9 +38,12 @@ public class MainGameTest{
     /**
      * Tests that an InvalidNumberOfPlayersException is thrown when the number of players is less than 2.
      * @throws InvalidNumberOfPlayersException is thrown if the number of players is < 2 or > 8 (expected).
+     * @throws SetupOperationPrecedenceException is thrown if a Map instances has already been created (not expected).
      */
     @Test(expected = InvalidNumberOfPlayersException.class)
-    public void belowMin_NoOfPlayers_setupPlayersTest() throws InvalidNumberOfPlayersException{
+    public void belowMin_NoOfPlayers_setupPlayersTest() throws InvalidNumberOfPlayersException,
+            SetupOperationPrecedenceException{
+
         int n_players = 1;
         mainGame.setupPlayers(n_players);
     }
@@ -41,59 +51,86 @@ public class MainGameTest{
     /**
      * Tests that an InvalidNumberOfPlayersException is thrown when the number of players is greater than 8.
      * @throws InvalidNumberOfPlayersException is thrown if the number of players is < 2 or > 8 (expected).
+     * @throws SetupOperationPrecedenceException is thrown if a Map instances has already been created (not expected).
      */
     @Test(expected = InvalidNumberOfPlayersException.class)
-    public void aboveMax_NoOfPlayers_setupPlayersTest() throws InvalidNumberOfPlayersException{
+    public void aboveMax_NoOfPlayers_setupPlayersTest() throws InvalidNumberOfPlayersException,
+            SetupOperationPrecedenceException{
+
         int n_players = 9;
         mainGame.setupPlayers(n_players);
     }
 
     /**
-     * Tests that an InvalidNumberOfPlayersException is thrown when no players have been initialized.
-     * @throws InvalidNumberOfPlayersException is thrown if MainGame.players is null (expected).
-     * @throws InvalidMapSizeException is thrown if the map_size is invalid (not expected).
+     * Tests that a SetupOperationPrecedenceException is thrown when a Map has already been initialised before the Players.
+     * @throws SetupOperationPrecedenceException is thrown if a Map instances has already been created (expected).
+     * @throws InvalidNumberOfPlayersException is thrown if the number of players is < 2 or > 8 (not expected).
      */
-    @Test(expected = InvalidNumberOfPlayersException.class)
-    public void noPlayersInitialized_setupMapTest() throws InvalidNumberOfPlayersException, InvalidMapSizeException{
-        MainGame.players = null;
+    @Test(expected = SetupOperationPrecedenceException.class)
+    public void mapInitialized_beforePlayersTest() throws SetupOperationPrecedenceException,
+            InvalidNumberOfPlayersException{
 
-        mainGame.setupMap(8);
+        MainGame.map = new BasicMap(tiles);
+        mainGame.setupPlayers(5);
+    }
+
+    /**
+     * Tests that a SetupOperationPrecedenceException is thrown when no players have been initialized before Map init.
+     * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (expected).
+     * @throws InvalidMapSizeException is thrown if the map_size is invalid (not expected).
+     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
+     */
+    @Test(expected = SetupOperationPrecedenceException.class)
+    public void noPlayersInitialized_setupMapTest() throws SetupOperationPrecedenceException, InvalidMapSizeException,
+            IllegalArgumentException{
+
+        MainGame.players = null;
+        mainGame.setupMap("basic",8);
     }
 
     /**
      * Tests that an InvalidMapSizeException is thrown when the map_size is less than 5.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
-     * @throws InvalidNumberOfPlayersException is thrown if MainGame.players is null (not expected).
+     * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
+     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void belowMin_MapSize_setupMapTest() throws InvalidMapSizeException, InvalidNumberOfPlayersException{
+    public void belowMin_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
+            IllegalArgumentException{
+
         int map_size = 4;
-        mainGame.setupMap(map_size);
+        mainGame.setupMap("basic", map_size);
     }
 
     /**
      * Tests that an InvalidMapSizeException is thrown when the map_size is greater than 50.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
-     * @throws InvalidNumberOfPlayersException is thrown if MainGame.players is null (not expected).
+     * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
+     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void aboveMax_MapSize_setupMapTest() throws InvalidMapSizeException, InvalidNumberOfPlayersException{
+    public void aboveMax_MapSize_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
+            IllegalArgumentException{
+
         int map_size = 51;
-        mainGame.setupMap(map_size);
+        mainGame.setupMap("basic", map_size);
     }
 
     /**
      * Tests that an InvalidMapSizeException is thrown when the map_size is greater than 5 but less than 8 for 5 or more
      * initialized players.
      * @throws InvalidMapSizeException is thrown if the map_size is invalid (expected).
-     * @throws InvalidNumberOfPlayersException is thrown if MainGame.players is null (not expected).
+     * @throws SetupOperationPrecedenceException is thrown if MainGame.players is null (not expected).
+     * @throws IllegalArgumentException is thrown when the map_type specified is not defined by MapCreator (not expected).
      */
     @Test(expected = InvalidMapSizeException.class)
-    public void smallMap_Min5Players_setupMapTest() throws InvalidMapSizeException, InvalidNumberOfPlayersException{
+    public void smallMap_Min5Players_setupMapTest() throws InvalidMapSizeException, SetupOperationPrecedenceException,
+            IllegalArgumentException{
+
         MainGame.players = new Player[5];
 
         int map_size = 5;
-        mainGame.setupMap(map_size);
+        mainGame.setupMap("basic", map_size);
     }
 
     @After
