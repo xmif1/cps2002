048d1b7009507ec47c183a93a24db2c38e295954
Domenico Agius <domenico.agius.18@um.edu.mt>
2020-05-17 17:21:01 +0200

    Added more detailed JavaDocs description of MapCreator class.

--- src/main/java/com/xd/cps2002/map/MapCreator.java
+++ src/main/java/com/xd/cps2002/map/MapCreator.java
@@ -1,7 +1,25 @@
 package com.xd.cps2002.map;
 
 /** The {@code MapCreator} class is used to create different types of {@link Map} objects. This class is the
- * "creator" class used to implement the factory design pattern.
+ * "creator" class used to implement the static factory design pattern.
+ *
+ * Using the static factory design pattern, the creation process of different types of maps is abstracted away from
+ * other classes. Hence, this allows the code to support new map types without necessarily needing to change the code
+ * where the {@link Map} sub-class is instantiated.
+ *
+ * Moreover, since the factory method used to create {@link Map} instances is {@code static}, the function can also
+ * simultaneously restrict the number of map instances that can be created to a single instance. In effect, the class
+ * also acts similar to a singleton, and creates and stores a {@link Map} object which is referred to throughout the
+ * runtime of the program.
+ *
+ * The advantage of including the logic to limit the creation of new instances in the {@link MapCreator} rather than
+ * {@link Map} is two-fold. First of all, the functionality to implement this restriction is only implemented once
+ * rather than having to implement it for each and every concrete instance of the {@link Map} class. Secondly, since the
+ * {@link Map} subclasses do not need to be singletons, they also become far more easy to test as a result. That being
+ * said, to allow only the {@link MapCreator} class to initialize concrete implementations of the {@link Map} class, the
+ * subclasses' constructors need to have {@code protected} rather {@code private} access modifiers. However, this access
+ * modifier is still flexible enough to allows unit tests in the same package as the classes to reinitialize {@link Map}
+ * instances between unit tests.
  *
  * @author Domenico Agius
  */
