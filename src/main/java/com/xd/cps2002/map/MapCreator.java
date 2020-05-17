4a2de5ec67324b4074c88702c33d42fe3e27cabe
domenico <domenico.agius.18@um.edu.mt>
2020-05-17 12:06:02 +0200

    Changed MapCreator.createMap factory method to only create a single map instance, no matter how many times it is called.

--- src/main/java/com/xd/cps2002/map/MapCreator.java
+++ src/main/java/com/xd/cps2002/map/MapCreator.java
@@ -7,23 +7,40 @@ package com.xd.cps2002.map;
  */
 public class MapCreator {
 
+    /**
+     * The {@code instance} member stores the Singleton instance of the map accessed by classes outside of the
+     * {@link com.xd.cps2002.map} package.
+     *
+     * @implNote  This is declared as {@code protected} instead of {@code private} to allow unit tests in the same
+     * package to easily reset the instance between tests. However, external methods still are not given direct access
+     * to this member.
+     */
+    protected static Map instance;
+
     /** Factory method used to create different types of {@link Map} objects.
      * @param mapType A string representing the type of map to be created.
      * @param size size of the map to be created
      * @return A Map object with the type represented by {@code mapType}
+     *
+     * @apiNote This method creates a new {@link Map} instance only the first time it is called. This means that the
+     * method is guaranteed to return a map of the requested type only the first time it is called.
      * */
     public static Map createMap(String mapType, int size) {
-        Map map;
-
         // Change mapType to lowercase to avoid having case sensitivity
         mapType = mapType.toLowerCase();
 
-        switch (mapType) {
-            case "basic":
-                map = new BasicMap(size); break;
-            default:
-                throw new IllegalArgumentException("Invalid map type.");
+        // If the singleton instance has not been created yet, create one according to the map type specified in the
+        // parameter "mapType"
+        if(instance == null) {
+            switch (mapType) {
+                case "basic":
+                    instance = new BasicMap(size); break;
+                default:
+                    throw new IllegalArgumentException("Invalid map type.");
+            }
         }
-        return map;
+
+        // Return the singleton instance
+        return instance;
     }
 }
